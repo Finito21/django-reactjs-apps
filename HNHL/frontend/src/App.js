a81503fab38ad6a4a5c88a12ad1210b56e69e112
Grzegorz Zięba <g.zieba21b@gmail.com>
2023-11-02 21:35:44 +0100

    Design-login-register

--- HNHL/frontend/src/App.js
+++ HNHL/frontend/src/App.js
@@ -9,6 +9,10 @@ import Categories from './components/Categories';
 import AllProducts from './components/AllProducts';
 import ProductDetail from './components/ProductDetail';
 import CategoryProducts from './components/CategoryProducts';
+import Checkout from './components/Checkout';
+
+import Register from './components/Customer/Register';
+import Login from './components/Customer/Login';
 
 
 function App() {
@@ -22,6 +26,10 @@ function App() {
       <Route path='/categories' element={<Categories/>}/>
       <Route path='/category/:category_slug/:category_id' element={<CategoryProducts/>}/>
       <Route path='/product/:product_slug/:product_id' element={<ProductDetail/>}/>
+      <Route path='/checkout' element={<Checkout/>}/>
+      <Route path='/customer/register' element={<Register/>}/>
+      <Route path='/customer/login' element={<Login/>}/>
+
 
     </Routes>
     <Footer/>
--- HNHL/frontend/src/components/ProductDetail.js
+++ HNHL/frontend/src/components/ProductDetail.js
@@ -1,27 +1,53 @@
 import { Link } from 'react-router-dom';
 import logo from '../logo.svg';
+import SingleProduct from './SingleProduct';
 function ProductDetail(){
     return (
         <section className='container mt-4'>
             <div className='row'>
                 <div className='col-4'>
-                    <img src={logo} className='img-thumbnail' alt="..."/>
-                </div>
+                    <div id="productThumbnailSlider" className="carousel carousel-dark slide" data-bs-ride="true">
+                        <div className="carousel-indicators">
+                            <button type="button" data-bs-target="#productThumbnailSlider" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                            <button type="button" data-bs-target="#productThumbnailSlider" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                            <button type="button" data-bs-target="#productThumbnailSlider" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                        </div>
+                        <div className="carousel-inner">
+                            <div className="carousel-item active">
+                                <img src={logo} className="img-thumbnail mb-5" alt="..."/>
+                            </div>
+                            <div className="carousel-item">
+                                <img src={logo} className="img-thumbnail mb-5" alt="..."/>
+                            </div>
+                            <div className="carousel-item">
+                                <img src={logo} className="img-thumbnail mb-5" alt="..."/>
+                            </div>
+                        </div>
+                        <button className="carousel-control-prev" type="button" data-bs-target="#productThumbnailSlider" data-bs-slide="prev">
+                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span className="visually-hidden">Previous</span>
+                        </button>
+                        <button className="carousel-control-next" type="button" data-bs-target="#productThumbnailSlider" data-bs-slide="next">
+                            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span className="visually-hidden">Next</span>
+                        </button>
+                    </div> 
+                </div>               
                 
             
                 <div className='col-8'>
                     <h3>Product Title</h3>
                     <p>djnakljxnckjznxkjcnzkjnxckjnzcxkjnxzckjnzzxczxczczxczxczxczxczxczxczczcxzcxzxczczcxzdsv c xcvcvxzzvCvzwfdsx SDfCscxezxcczcx</p>
-                    <h5 className='card-title'>Price 500</h5>
+                    <h5 className='img-title'>Price 500</h5>
                     <p className='mt-3'>
-                        <button title="Add to card" className='btn btn-primary'>
-                            <i class="fa-solid fa-cart-plus"></i>Add to Cart
+                        <button title="Add to img" className='btn btn-primary'>
+                            <i className="fa-solid fa-cart-plus"></i>Add to Cart
                             </button>
                         <button title="buy Now" className='btn btn-success ms-1'>
-                            <i class="fa-solid fa-bag-shopping"></i>Buy Now
+                            <i className="fa-solid fa-bag-shopping"></i>Buy Now
                             </button>    
                         <button title="Add to Wishlist" className='btn btn-danger ms-1'>
-                            <i class="fa fa-heart"></i>Wishlist</button>
+                            <i className="fa fa-heart"></i>Wishlist</button>
                     </p>
                     <hr/>
                     <div className='producttags mt-4'>
@@ -35,6 +61,50 @@ function ProductDetail(){
                     </div>
                 </div>
 
+            </div>
+
+            <h3 className='mt-5 mb-3'>Related Products</h3>
+
+            <div id="relatedProductsSlider" className="carousel carousel-dark slide" data-bs-ride="true">
+                <div className="carousel-indicators">
+                    <button type="button" data-bs-target="#relatedProductsSlider" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#relatedProductsSlider" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#relatedProductsSlider" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <div className='row mb-4'>
+                            <SingleProduct title="Django Project 1"/>
+                            <SingleProduct title="Django Project 2"/>
+                            <SingleProduct title="Django Project 3"/>
+                            <SingleProduct title="Django Project 4"/>
+                        </div>
+                    </div>
+                    <div className="carousel-item">
+                    <div className='row mb-4'>
+                            <SingleProduct title="Django Project 5"/>
+                            <SingleProduct title="Django Project 6"/>
+                            <SingleProduct title="Django Project 7"/>
+                            <SingleProduct title="Django Project 8"/>
+                        </div>
+                    </div>
+                    <div className="carousel-item">
+                    <div className='row mb-4'>
+                            <SingleProduct title="Django Project 9"/>
+                            <SingleProduct title="Django Project 10"/>
+                            <SingleProduct title="Django Project 11"/>
+                            <SingleProduct title="Django Project 12"/>
+                        </div>
+                    </div>
+                </div>
+                {/*<button className="carousel-control-prev" type="button" data-bs-target="#relatedProductsSlider" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target="#relatedProductsSlider" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+    </button>*/}
             </div>
 
 
