9bc59ceb9ed7be754fef30d63631a1d70c836461
Grzegorz Zięba <g.zieba21b@gmail.com>
2023-11-08 08:52:49 +0100

    nie działa

--- HNHL/backend_api/main/views.py
+++ HNHL/backend_api/main/views.py
@@ -16,7 +16,7 @@ class VendorDetail(generics.RetrieveUpdateDestroyAPIView):
 class ProductList(generics.ListCreateAPIView):
     queryset=models.Product.objects.all()
     serializer_class=serializers.ProductListSerializer
-    permission_class=pagination.PageNumberPagination
+    pagination_class=pagination.PageNumberPagination
 
 class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset=models.Product.objects.all()
--- HNHL/frontend/src/components/AllProducts.js
+++ HNHL/frontend/src/components/AllProducts.js
@@ -1,20 +1,34 @@
 import logo from '../logo.svg';
 import SingleProduct from './SingleProduct';
+import { useState,useEffect} from 'react';
 function AllProducts(){
+        const [products,setProducts]=useState([])
+
+        useEffect(()=>{
+            fetchData('http://127.0.0.1:8000/api/products/');
+           
+        });
+
+        function fetchData(baseurl){
+            fetch(baseurl)
+            .then((response)=> response.json())
+            .then((data)=> setProducts(data.results));
+    
+    
+        }
+
+    
+    
+
+
+
     return(
         <section className='container'>
             <h3 className='mb-4'>All Products</h3>
             <div className='row mb-4'>
-
-                <SingleProduct title="Django Project 1"/>
-                <SingleProduct title="Django Project 2"/>
-                <SingleProduct title="Django Project 3"/>
-                <SingleProduct title="Django Project 4"/>
-                <SingleProduct title="Django Project 5"/>
-                <SingleProduct title="Django Project 6"/>
-                <SingleProduct title="Django Project 7"/>
-                <SingleProduct title="Django Project 8"/>
-                <SingleProduct title="Django Project 9"/>
+                {
+                products.map((product) => <SingleProduct product={product}/>)
+                }
                 
             </div>
                 
